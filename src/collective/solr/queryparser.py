cfbf3523599d12236b41041bf372a744c04eb858
Gil Forcada <gforcada@gnome.org>
2015-12-09 16:48:35 +0100

    DO NOT MERGE

--- src/collective/solr/queryparser.py
+++ src/collective/solr/queryparser.py
@@ -124,7 +124,7 @@ def quote(term, textfield=False):
                 stack.current.append(Whitespace())
             elif isinstance(stack.current, list):
                 # We have whitespace with no grouping, insert group
-                new = Group('(', ')')
+                new = Group('"', '"')
                 new.extend(stack.current)
                 new.append(Whitespace())
                 stack.current[:] = []
