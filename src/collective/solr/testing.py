94b12ae870e834814116c9d6e5a8db31d5b06a0e
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-05-08 15:13:36 +0200

    set auto commit and commit within

--- src/collective/solr/testing.py
+++ src/collective/solr/testing.py
@@ -134,6 +134,8 @@ class CollectiveSolrLayer(PloneSandboxLayer):
         set_registry_record("collective.solr.active", self.solr_active)
         set_registry_record("collective.solr.port", self.solr_port)
         set_registry_record("collective.solr.base", self.solr_base)
+        set_registry_record('collective.solr.auto_commit', True)
+        set_registry_record('collective.solr.commit_within', 0)
 
     def tearDownPloneSite(self, portal):
         set_registry_record("collective.solr.active", False)
